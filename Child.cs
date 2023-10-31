ec6e9b844a8005d5d5adff29034cb57e9ebfbe53
Deep Choudhery <decho@microsoft.com>
2023-10-31 14:04:08 -0700

    added blazor CRUD scaffolding

---
+++ Child.cs
@@ -0,0 +1,7 @@
+public class Child
+{
+    public int ID { get; set; }
+    public string? Name { get; set; }
+    public bool Annoying { get; set; }
+    public DateTime? Birthday { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+using BlazorCRUD.Components;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<NewDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("NewDbContext") ?? throw new InvalidOperationException("Connection string 'NewDbContext' not found.")));
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();;
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
